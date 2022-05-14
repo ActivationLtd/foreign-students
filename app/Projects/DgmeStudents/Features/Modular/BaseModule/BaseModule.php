7c68403b1d5f70cef2eae5b58eb1c09b314e9643
RaihanS <raihan.act@gmail.com>
2022-05-15 02:39:52 +0600

    #

--- app/Projects/DgmeStudents/Features/Modular/BaseModule/BaseModule.php
+++ app/Projects/DgmeStudents/Features/Modular/BaseModule/BaseModule.php
@@ -2,7 +2,14 @@
 
 namespace App\Projects\DgmeStudents\Features\Modular\BaseModule;
 
-use App\Mainframe\Features\Modular\BaseModule\BaseModule as MainframeBaseModule;
+use App\Mainframe\Features\Core\Traits\Validable;
+use App\Mainframe\Features\Modular\BaseModule\Traits\ModularTrait;
+use App\Mainframe\Features\Multitenant\GlobalScope\CheckTenantScope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Models\Audit;
+use Watson\Rememberable\Rememberable;
 
 /**
  * App\Projects\DgmeStudents\Features\Modular\BaseModule\BaseModule
@@ -29,7 +36,124 @@ use App\Mainframe\Features\Modular\BaseModule\BaseModule as MainframeBaseModule;
  * @method static \Illuminate\Database\Query\Builder|BaseModule withTrashed()
  * @method static \Illuminate\Database\Query\Builder|BaseModule withoutTrashed()
  */
-class BaseModule extends MainframeBaseModule
+class BaseModule extends Model implements Auditable
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Include Mainframe module traits
+    |--------------------------------------------------------------------------
+    */
+    use SoftDeletes,                // Laravel default trait to enable soft delete
+        Rememberable,               // Third party plugin to cache model query
+        \OwenIt\Auditing\Auditable, // 3rd party audit log
+        ModularTrait,               // Mainframe modular features.
+        Validable                   // Allow validation
+        ;
 
+    /*
+    |--------------------------------------------------------------------------
+    | Module definitions
+    |--------------------------------------------------------------------------
+    */
+    protected $moduleName = ''; // Note: demo module name to create ide-helper doc block
+
+    /*
+    |--------------------------------------------------------------------------
+    | Attributes
+    |--------------------------------------------------------------------------
+    */
+    /**
+     * Date fields
+     *
+     * @var string[]
+     */
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * Hidden in serialized output/json
+     *
+     * @var string[]
+     */
+    protected $hidden = ['project_id', 'tenant_id', 'slug', 'deleted_by', 'deleted_at'];
+
+    /**
+     * Attributes to exclude from the Audit.
+     *
+     * @var array
+     */
+    protected $auditExclude = ['updated_at',];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Code: Spread configs
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Define the spread attribute mapping that link to a another model.
+     * Note: Table field must follow *_model_ids, i.e. visited_country_ids, active_group_ids
+     *
+     * @var array
+     */
+    protected $spreadFields = [
+        // 'group_ids' => Group::class,
+    ];
+
+    /**
+     * Define the tag attributes of the model that will be saved in spreads table.
+     *
+     * @var array
+     */
+    protected $tagFields = [
+        // 'first_name',
+        // 'group_ids',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Code: Tenant configs
+    |--------------------------------------------------------------------------
+    */
+    /**
+     * Enable tenant context
+     *
+     * @var bool
+     */
+    protected $tenantEnabled = false;
+
+    /**
+     * If true then tenants will be able to see items where tenant_id=0
+     *
+     * @var bool
+     */
+    protected $showGlobalTenantElements = true;
+
+    /**
+     * If true then tenants will be able to see items where tenant_id=null
+     *
+     * @var bool
+     */
+    protected $showNonTenantElements = true;
+
+    /*
+    |--------------------------------------------------------------------------
+    | Boot method and model events.
+    |--------------------------------------------------------------------------
+    */
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Skip audit log if there is no change
+        Audit::creating(function (Audit $model) {
+            if (empty($model->old_values) && empty($model->new_values)) {
+                return false;
+            }
+        });
+
+        // Add tenant scope to model if current user() belongs to a tenant
+        if (user()->ofTenant()) {
+            static::addGlobalScope(new CheckTenantScope);
+        }
+    }
 }
