c5d8f48ad75094862d10114f5341569c90a58be9
RaihanS <raihan.act@gmail.com>
2022-05-14 14:51:19 +0600

    email template fix

---
+++ resources/views/projects/dgme-students/layouts/email/includes/header.blade.php
@@ -0,0 +1,16 @@
+<table class="no-border no-padding" style="width:500px; margin: 0 auto;">
+    <tr>
+        <td>
+            <img src="{{asset('projects/dgme-students/images/bangladesh.png')}}"
+                 style="width: 120px; float: right; padding-right: 15px" alt="">
+        </td>
+        <td>
+            <h1 style="text-align: center">{{__('common.agency_full_name')}}</h1>
+            <h2 style="text-align: center">{{__('common.gov_of_bangladesh')}}</h2>
+        </td>
+        <td>
+            <img src="{{asset('projects/dgme-students/images/dgme.png')}}" class="pull-right"
+                 style="margin-top: 2%; border:none!important;width: 120px;" alt="">
+        </td>
+    </tr>
+</table>
