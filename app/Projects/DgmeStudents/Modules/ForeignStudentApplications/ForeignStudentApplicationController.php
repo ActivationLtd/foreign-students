6a0b6cf369197e166719a0c8516dbbbd81e9bb0a
sanjid29 <sanjidhabib@gmail.com>
2022-05-22 23:41:58 +0600

    https://activationltd.atlassian.net/browse/DGME-88

--- app/Projects/DgmeStudents/Modules/ForeignStudentApplications/ForeignStudentApplicationController.php
+++ app/Projects/DgmeStudents/Modules/ForeignStudentApplications/ForeignStudentApplicationController.php
@@ -93,5 +93,6 @@ class ForeignStudentApplicationController extends ModularController
             'application' => $foreignStudentApplication,
             'content' => $contentQrCode,
         ]);
+
     }
 }
