27b3a5afe380513992c025ba9052d6d75bea8fd2
sanjid29 <sanjidhabib@gmail.com>
2022-06-05 21:42:35 +0600

    https://activationltd.atlassian.net/browse/DGME-88

--- resources/views/projects/dgme-students/modules/foreign-student-applications/form/default.blade.php
+++ resources/views/projects/dgme-students/modules/foreign-student-applications/form/default.blade.php
@@ -157,23 +157,10 @@ if (user()->isAdmin()) {
             <div class="clearfix"></div>
 
             <div class="col-md-12 no-padding-l">
-                {{--Education List--}}
-                <?php
-                $datatable = new \App\Projects\DgmeStudents\Datatables\ApplicationExaminationDatatable();
-                $datatable->addUrlParam(['foreign_student_application_id' => $element->id]);
-                $datatable->bPaginate = false;
-                ?>
-                <h3>6. Beginning with Matriculation/O Level or equivalent examinations list your examinations</h3>
-                @include('mainframe.layouts.module.grid.includes.datatable',['datatable'=>$datatable])
-                <div class="clearfix"></div>
-                @if($view->showExaminationCreateButton())
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#examinationModal">
-                        Add
-                    </button>
-                @endif
+                @include('projects.dgme-students.modules.foreign-student-applications.form.includes.application-examination-modal')
             </div>
             <div class="col-md-12 no-padding-l">
-               @include('projects.dgme-students.modules.foreign-student-applications.form.includes.language-proficiency-modal')
+                @include('projects.dgme-students.modules.foreign-student-applications.form.includes.language-proficiency-modal')
             </div>
             <div class="clearfix"></div>
             <h3>8. Payment Info</h3>
@@ -222,7 +209,7 @@ if (user()->isAdmin()) {
 
 @section('content-bottom')
     @parent
-    @if($element->id)
+    @if($element->isCreated())
         <div class="col-md-12 no-padding-l">
             <h3>Upload Documents</h3>
         </div>
@@ -246,51 +233,6 @@ if (user()->isAdmin()) {
             @include('form.uploads',['var'=>['limit'=>1,'type'=>\App\Upload::TYPE_HSC_EQUIVALENT,'zip_download'=>false]])
         </div>
     @endif
-
-    @if($element->id && $view->showExaminationCreateButton())
-        <div class="modal fade" id="examinationModal" tabindex="-1" role="dialog"
-             aria-labelledby="examinationModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <form id="applicationExaminationForm" name="applicationExaminationForm"
-                          action="{{route('foreign-application-examinations.store')}}"
-                          method="POST">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Add Examination</h5>
-                            <button id="applicationExaminationModalCloseButton" type="button" class="close"
-                                    data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <input name="_token" type="hidden" value="{{csrf_token()}}">
-                            <input name="foreign_student_application_id" type="hidden" value="{{$element->id}}">
-                            <input name="user_id" type="hidden" value="{{$element->user_id}}">
-                            <div class="clearfix"></div>
-                            @include('form.select-array',['var'=>['name'=>'examination_type','label'=>'O Level/ A Level Equivalent', 'options'=>($examinationTypes),'div'=>'col-md-12']])
-                            @include('form.text',['var'=>['name'=>'examination_name','label'=>'Examination','div'=>'col-md-12']])
-                            @include('form.number',['var'=>['name'=>'passing_year','label'=>'Passing Year','div'=>'col-md-6']])
-                            @include('form.textarea',['var'=>['name'=>'subjects','label'=>'Subjects Taken','div'=>'col-md-12']])
-                            @include('form.text',['var'=>['name'=>'certificate_name','label'=>'Certificate','div'=>'col-md-12']])
-                            <input name="redirect_success" type="hidden"
-                                   value="{{route('foreign-student-applications.edit',$element->id)}}"/>
-                            <input name="redirect_fail" type="hidden"
-                                   value="{{route('foreign-student-applications.edit',$element->id)}}"/>
-                            {{--<input name="redirect_fail" type="hidden" value="{{URL::full()}}"/>--}}
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="modal-footer">
-                            <button id="applicationExaminationFormButton" name="applicationExaminationFormButton"
-                                    type="submit" class="btn btn-primary">Add
-                                Examination
-                            </button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    @endif
 @endsection
 
 @section('js')
@@ -299,6 +241,5 @@ if (user()->isAdmin()) {
     <script type="text/javascript">
         $('select[id=dob_country_id]').select2();
         $('select[id=domicile_country_id]').select2();
-
     </script>
 @endsection
