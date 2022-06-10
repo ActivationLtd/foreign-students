5896a4c03d0739a350c2d2c919fca2ff22d3efae
RaihanS <raihan.act@gmail.com>
2022-06-11 02:37:01 +0600

    #

--- app/Mainframe/Features/Datatable/Traits/DatatableTrait.php
+++ app/Mainframe/Features/Datatable/Traits/DatatableTrait.php
@@ -25,8 +25,10 @@ trait DatatableTrait
     public function columns()
     {
         return [
-            [$this->table.'.id', 'id', 'ID'], [$this->table.'.name', 'name', 'Name'],
-            ['updater.name', 'user_name', 'Updater'], [$this->table.'.updated_at', 'updated_at', 'Updated at'],
+            [$this->table.'.id', 'id', 'ID'],
+            [$this->table.'.name', 'name', 'Name'],
+            ['updater.name', 'user_name', 'Updater'],
+            [$this->table.'.updated_at', 'updated_at', 'Updated at'],
             [$this->table.'.is_active', 'is_active', 'Active'],
         ];
     }
